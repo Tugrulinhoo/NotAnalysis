74954847db090b5ad9a033349febf096ac16af2a
Tugrulinhoo <tugrulacar583@gmail.com>
2026-08-08 01:48:07 +0300

    Add analysis option and update menu choices

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
+
 from TabloDuzenleyici import (
     tabloyu_getir,
     Ogrenci_ekle,
     Ogrenci_sil,
     not_degisim,
-    ad_degisim
+    ad_degisim,
+    analiz
 )
 
 
@@ -17,38 +19,72 @@ print("""
 [3] Öğrenci Sil
 [4] Not Değiştir
 [5] İsim Değiştir
-[6] Çıkış
+[6] Analiz
+[7] Çıkış
 """)
 
 
 while True:
 
     try:
-        secim = int(input("Seçiminiz: "))
+        secim = int(
+            input("\nSeçiminiz: ")
+        )
 
     except ValueError:
         print("Lütfen sayı giriniz.")
         continue
 
+
     if secim == 1:
+
+        print("\n--- TABLO ---")
         print(tabloyu_getir())
+
 
     elif secim == 2:
+
         Ogrenci_ekle()
+
+        print("\nGüncel Tablo:")
         print(tabloyu_getir())
 
+
     elif secim == 3:
+
         Ogrenci_sil()
 
+        print("\nGüncel Tablo:")
+        print(tabloyu_getir())
+
+
     elif secim == 4:
+
         not_degisim()
 
+        print("\nGüncel Tablo:")
+        print(tabloyu_getir())
+
+
     elif secim == 5:
+
         ad_degisim()
 
+        print("\nGüncel Tablo:")
+        print(tabloyu_getir())
+
+
     elif secim == 6:
-        print("Program kapatılıyor...")
+
+        analiz()
+
+
+    elif secim == 7:
+
+        print("\nProgram kapatılıyor...")
         break
 
+
     else:
+
         print("Geçersiz seçenek.")
